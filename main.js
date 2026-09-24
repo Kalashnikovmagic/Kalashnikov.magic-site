6c13ea78e6d8b3364a87d6a538292e1b703be7dd
Kalashnikovmagic <akalashnikov26@gmail.com>
2026-09-24 09:35:01 +0300

    Remove remaining unused hero state

--- main.js
+++ main.js
@@ -11,12 +11,7 @@ const heroContext=heroCanvas?.getContext('2d',{alpha:false});
 const HERO_FRAME_COUNT=87;
 const HERO_FRAME_PATH='assets/hero/frame_';
 const heroFrames=new Array(HERO_FRAME_COUNT);
-let heroFrameWidth=0;
-let heroFrameHeight=0;
 let heroCurrentFrame=-1;
-let heroLastDrawnWidth=0;
-let heroLastDrawnHeight=0;
-let heroLoaded=false;
 
 function heroFrameUrl(index){
   return HERO_FRAME_PATH+String(index+1).padStart(4,'0')+'.webp';
